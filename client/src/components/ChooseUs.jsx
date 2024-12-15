62e56935eb4507dba732b18da1be9baebd2b55f1
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-12-15 06:04:42 +0000

    Add ChooseUs and Clients components, index.css, index.js, and logo.svg

---
+++ client/src/components/ChooseUs.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+export default function ChooseUs() {
+    return (
+        <>
+            <h1 className="text-xl font-bold mb-2">Get HomeWise.ai Assistant</h1>
+            <h1 className="text-xl text-gray-500 font-semibold mb-10">The Next Era of Intelligent Living</h1>
+            <div className="join join-vertical w-full">
+                <div className="collapse collapse-plus join-item border border-base-300">
+                    <input type="radio" name="my-accordion-4" defaultChecked />
+                    <div className="collapse-title text-xl font-medium">
+                    Live smarter—HomeWise takes care of the little things in life so you can focus on what is important.
+                    </div>
+                    <div className="collapse-content">
+                        <p>hello</p>
+                    </div>
+                </div>
+                <div className="collapse collapse-plus join-item border border-base-300">
+                    
+                    <input type="radio" name="my-accordion-4" />
+                    <div className="collapse-title text-xl font-medium">
+                    Delight Your Customers
+                    </div>
+                    <div className="collapse-content">
+                        <p>Home control</p>
+                    </div>
+                </div>
+                <div className="collapse collapse-plus join-item border border-base-300">
+                    <input type="radio" name="my-accordion-4" />
+                    <div className="collapse-title text-xl font-medium">
+                    Take Control of Your Environment
+                    </div>
+                    <div className="collapse-content">
+                        <p>hello</p>
+                    </div>
+                </div>
+                <div className="collapse collapse-plus join-item border border-base-300">
+                    <input type="radio" name="my-accordion-4" />
+                    <div className="collapse-title text-xl font-medium">
+                    Automate Your Lifestyle
+                    </div>
+                    <div className="collapse-content">
+                        <p>hello</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
