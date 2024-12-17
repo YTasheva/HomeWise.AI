da2bbc3abc19aff4f4782a9396ad69b3c256a9be
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-12-17 18:55:28 +0000

    Refactor Slide and Statistics components for improved readability and consistency

--- client/src/components/Slide.jsx
+++ client/src/components/Slide.jsx
@@ -1,18 +1,34 @@
-import React from 'react'
-import { ArrowLongLeftIcon } from '@heroicons/react/24/solid'
-import { ArrowLongRightIcon } from '@heroicons/react/24/solid'
+import React from "react";
+import { ArrowLongLeftIcon } from "@heroicons/react/24/solid";
+import { ArrowLongRightIcon } from "@heroicons/react/24/solid";
 
 export default function Slide() {
   return (
     <>
       <div className="flex relative">
         <div className="w-1/2 p-4 pl-32 bg-white mt-10 relative z-10 h-max">
-          <h1 className="text-2xl font-medium mb-2"><span className='text-yellow font-bold'>|</span> SUPERCHARGED ASSISTANT</h1>
-          <h1 className="text-6xl font-semibold mb-2">Simple. Private. Magical.</h1>
+          <h1 className="text-2xl font-medium mb-2">
+            <span className="text-yellow font-bold">|</span> SUPERCHARGED
+            ASSISTANT
+          </h1>
+          <h1 className="text-6xl font-semibold mb-2">
+            Simple. Private. Magical.
+          </h1>
           <h1 className="text-6xl font-bold mb-2 text-yellow">HomeWise App</h1>
-          <p className="text-gray-700">The mission at HomeWise.ai is to solve the user experience, interoperability, and innovation challenges in the smart home. Enjoy efficient smart home accessibility with the HomeWise App or conveniently set the mood with conversational voice commands. HomeWise knows its role and continues to adapt, enabling users to freely interact with their surroundings and automate their lifestyle in whatever way feels easiest. HomeWise alleviates privacy concerns and customization challenges so that every client can freely enjoy their smart home experience.</p>
-          <a className="btn bg-yellow text-white mt-10 rounded-none">Discover More</a>
-
+          <p className="text-gray-700">
+            The mission at HomeWise.ai is to solve the user experience,
+            interoperability, and innovation challenges in the smart home. Enjoy
+            efficient smart home accessibility with the HomeWise App or
+            conveniently set the mood with conversational voice commands.
+            HomeWise knows its role and continues to adapt, enabling users to
+            freely interact with their surroundings and automate their lifestyle
+            in whatever way feels easiest. HomeWise alleviates privacy concerns
+            and customization challenges so that every client can freely enjoy
+            their smart home experience.
+          </p>
+          <a className="btn bg-yellow text-white mt-10 rounded-none">
+            Discover More
+          </a>
         </div>
         <div className="w-1/2 -ml-16">
           <div className="carousel w-full">
@@ -29,9 +45,13 @@ export default function Slide() {
       </div>
       <div className="flex justify-center items-center h-auto w-max bg-yellow p-2">
         <p className="text-white px-2">1 / 2</p>
-        <a href=""><ArrowLongLeftIcon className="h-8 w-8 text-white" /></a>
-        <a href=""><ArrowLongRightIcon className="h-8 w-8 text-white" /></a>
+        <a href="">
+          <ArrowLongLeftIcon className="h-8 w-8 text-white" />
+        </a>
+        <a href="">
+          <ArrowLongRightIcon className="h-8 w-8 text-white" />
+        </a>
       </div>
     </>
-  )
+  );
 }
