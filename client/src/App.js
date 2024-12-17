e1b0cf0218ca85c794e71e6f4c8fda6f91c381c3
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-12-17 18:55:57 +0000

    Refactor App and index components for improved readability and consistency

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,10 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from 'react-router-dom';
-import Home from './pages/home';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Upload from './pages/UploadDoc';
-import Contact from './pages/Contact';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Upload from "./pages/UploadDoc";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+//import 'bootstrap/js/src/collapse';
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-// import 'bootstrap/js/src/collapse';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
 
 reportWebVitals();
