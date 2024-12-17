27205fae149eb1f95419708adbe2d39a52787ffc
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-12-17 18:55:37 +0000

    Refactor Testimonials and Welcome components for improved readability and consistency

--- client/src/components/Testimonials.jsx
+++ client/src/components/Testimonials.jsx
@@ -1,26 +1,34 @@
-import React from 'react'
-import ChooseUs from './ChooseUs'
+import React from "react";
+import ChooseUs from "./ChooseUs";
 
 export default function Testimonials() {
-    return (
-        <>
-            <div className="flex justify-center space-x-44 text-left mt-20">
-                <div className="w-1/2 ml-16">
-                    <h1 className="text-xl font-bold mb-2 ml-16">OUR CUSTOMER STORIES</h1>
-                    <h1 className="text-xl text-gray-500 font-semibold mb-10 ml-16">CHECK OUT OUR CASE STUDIES HERE</h1>
-                    <div className="chat chat-end">
-                        <div className="chat-bubble bg-[#EEEEEE] text-gray-600 rounded-none p-16">I’ve been fascinated with HomeWise.ai. They’ve received a ton of attention from our industry trade press and produced a lot of buzz and momentum. I’m all for seeing our industry lead the charge with new solutions, but I wanted to experience these products for myself before making up my mind. Thanks HomeWise!</div>
-                        <div className="chat-footer">
-                            <p className="text-xl font-bold">John Bentley</p>
-                            <p className="text-xl font-light">Managing Director</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="w-1/2">
-                    <ChooseUs />
-                </div>
+  return (
+    <>
+      <div className="flex justify-center space-x-44 text-left mt-20">
+        <div className="w-1/2 ml-16">
+          <h1 className="text-xl font-bold mb-2 ml-16">OUR CUSTOMER STORIES</h1>
+          <h1 className="text-xl text-gray-500 font-semibold mb-10 ml-16">
+            CHECK OUT OUR CASE STUDIES HERE
+          </h1>
+          <div className="chat chat-end">
+            <div className="chat-bubble bg-[#EEEEEE] text-gray-600 rounded-none p-16">
+              I’ve been fascinated with HomeWise.ai. They’ve received a ton of
+              attention from our industry trade press and produced a lot of buzz
+              and momentum. I’m all for seeing our industry lead the charge with
+              new solutions, but I wanted to experience these products for
+              myself before making up my mind. Thanks HomeWise!
             </div>
-        </>
-    )
+            <div className="chat-footer">
+              <p className="text-xl font-bold">John Bentley</p>
+              <p className="text-xl font-light">Managing Director</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-1/2">
+          <ChooseUs />
+        </div>
+      </div>
+    </>
+  );
 }
--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,23 +1,33 @@
-import React from 'react'
+import React from "react";
 
 export default function Welcome() {
   return (
-<div className="flex mt-40 px-8">
+    <div className="flex mt-40 px-8">
+      <div className="w-1/2">
+        <div className="h-100">
+          <img src="./welcome.png" className="w-full object-cover" />
+        </div>
+      </div>
 
-<div className="w-1/2">
-<div className="h-100">
-<img src="./welcome.png" className="w-full object-cover" />
-</div>
-</div>
-
-<div className="w-1/2 pl-40">
-<h1 className="text-5xl font-semibold mb-2">SPEAK NATURALLY</h1>
-<h1 className="text-5xl font-semibold mb-2">HomeWise <span className="text-red">Understands Natural Speach</span></h1>
-<p className="text-xl text-gray-600 mt-5">Your personal Chef.  HomeWise GPT now offers the assistance of an expert sous chef. Enjoy access to delicious recipes, culinary instruction, and kitchen inspiration thanks to HomeWise! Proactive
-recommendations developed by artificial intelligence.</p>
-<p className="text-xl text-gray-500 mt-5">Interested in HomeWise? Reach out and we’ll connect you with a local Home Technology Professional.</p>
-<a className="btn bg-[#F5F5F5] text-[#4A6E7E] font-bold mt-10 rounded-none">Say What's On Your Mind</a>
-</div>
-</div>
-  )
+      <div className="w-1/2 pl-40">
+        <h1 className="text-5xl font-semibold mb-2">SPEAK NATURALLY</h1>
+        <h1 className="text-5xl font-semibold mb-2">
+          HomeWise <span className="text-red">Understands Natural Speach</span>
+        </h1>
+        <p className="text-xl text-gray-600 mt-5">
+          Your personal Chef. HomeWise GPT now offers the assistance of an
+          expert sous chef. Enjoy access to delicious recipes, culinary
+          instruction, and kitchen inspiration thanks to HomeWise! Proactive
+          recommendations developed by artificial intelligence.
+        </p>
+        <p className="text-xl text-gray-500 mt-5">
+          Interested in HomeWise? Reach out and we’ll connect you with a local
+          Home Technology Professional.
+        </p>
+        <a className="btn bg-[#F5F5F5] text-[#4A6E7E] font-bold mt-10 rounded-none">
+          Say What's On Your Mind
+        </a>
+      </div>
+    </div>
+  );
 }
